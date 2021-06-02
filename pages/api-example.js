35e285c0df51dca79c7bcd7505af6dc45e37288b
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-06-02 13:28:43 +0530

    setted tailwind

--- pages/api-example.js
+++ pages/api-example.js
@@ -1,17 +1,24 @@
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 
-export default function Page () {
+export default function Page() {
   return (
     <Layout>
       <h1>API Example</h1>
       <p>The examples below show responses from the example API endpoints.</p>
-      <p><em>You must be signed in to see responses.</em></p>
+      <p>
+        <em>You must be signed in to see responses.</em>
+      </p>
       <h2>Session</h2>
       <p>/api/examples/session</p>
-      <iframe src="/api/examples/session"/>
+      <iframe src='/api/examples/session' />
       <h2>JSON Web Token</h2>
       <p>/api/examples/jwt</p>
-      <iframe src="/api/examples/jwt"/>
+      <iframe src='/api/examples/jwt' />
+
+      <div className='bg-white dark:bg-gray-800'>
+        <h1 className='text-gray-900 dark:text-white'>Dark mode is here!</h1>
+        <p className='text-gray-600 dark:text-gray-300'>Lorem ipsum...</p>
+      </div>
     </Layout>
-  )
+  );
 }
